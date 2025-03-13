065f4e90e18481173d91bd943a3decd8b1a09dee
paulr <paulrosselet@gmail.com>
2025-03-13 14:03:32 +0100

    agglo

--- agglo.R
+++ agglo.R
@@ -18,12 +18,27 @@ data_tcu_tempo <- subset(full_data_tcu, full_data_tcu$var_id==181) #On récup le
 data_tcu <- merge(x=data_tcu, y=data_tcu_tempo, by = c("pm_id", "enq_id"))
 data_tcu <- rename(data_tcu, POP=dat_valeur)
 
-data_tcu <- data_tcu[,c("enq_id","pm_id","LC","POP","dat_date.x","pm_nom","pm_rang","pm_sigle")]
+data_tcu_tempo <- subset(full_data_tcu, full_data_tcu$var_id==5724) #On récup le nombre d'arret
+data_tcu <- merge(x=data_tcu, y=data_tcu_tempo, by = c("pm_id", "enq_id"))
+data_tcu <- rename(data_tcu, NB_ARR=dat_valeur)
+
+data_tcu <- data_tcu[,c("enq_id","pm_id","LC","POP", "NB_ARR","dat_date.x","pm_nom","pm_rang","pm_sigle")]
+
+
+data_tcu_tempo <- subset(full_data_tcu, full_data_tcu$var_id==1810) #On récup la superficie
+data_tcu <- merge(x=data_tcu, y=data_tcu_tempo, by = c("pm_id", "enq_id"))
+data_tcu <- rename(data_tcu, SUP=dat_valeur)
+
+data_tcu <- data_tcu[,c("enq_id","pm_id","LC","POP", "NB_ARR", "SUP","dat_date.x","pm_nom","pm_rang","pm_sigle")]
 
 data_tcu$LC <- as.numeric(data_tcu$LC)
 data_tcu$POP <- as.numeric(data_tcu$POP)
+data_tcu$SUP <- as.numeric(data_tcu$SUP)
+data_tcu$NB_ARR <- as.numeric(data_tcu$NB_ARR)
 
 data_tcu$lc_par_hab <-1000* data_tcu$LC / data_tcu$POP
+data_tcu$arr_par_sup <- data_tcu$NB_ARR / data_tcu$SUP
+
 
 exploitants <- read.csv("exploitants.csv", sep = ",", stringsAsFactors = FALSE)
 donnees_tcu_agglo <- inner_join(data_tcu, exploitants, by="pm_id")
